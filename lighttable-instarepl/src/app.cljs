b7ee2fdf5ea6f4e9c1caee24a81ceaf5e15ab992
Daniel Ribeiro <danrbr@gmail.com>
2013-08-29 04:48:04 -0700

    fixing wrong form

--- lighttable-instarepl/src/app.cljs
+++ lighttable-instarepl/src/app.cljs
@@ -3,7 +3,7 @@
 
 (js/alert (world/default-string))
 
-(defn hack-body [] (set! (.. js/document.body.innerHTML -innerHTML) "<h1>This has been hacked 2/h1>"))
+(defn hack-body [] (set! (.. js/document -body -innerHTML) "<h1>This has been hacked 2/h1>"))
 
 ;umcoment line below and command/control enter it to evaluate it
 
