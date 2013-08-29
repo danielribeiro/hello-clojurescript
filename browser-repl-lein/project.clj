032190df1ccb294536e2860029735e588bca2f6a
Daniel Ribeiro <danrbr@gmail.com>
2013-08-28 23:57:08 -0700

    updating project files

--- browser-repl-lein/project.clj
+++ browser-repl-lein/project.clj
@@ -1,4 +1,4 @@
-(defproject browser-repl-lein "0.1.0-SNAPSHOT"
+(defproject browser-repl-lein "0.1.0"
   :dependencies [[org.clojure/clojure "1.5.1"]]
   :plugins [[lein-cljsbuild "0.3.2"]]
   :cljsbuild
